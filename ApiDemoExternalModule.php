90fe8b9085a92dc47cf46e884bd15b7ff0d8e129
Günther Rezniczek <g.rezniczek@gmail.com>
2023-11-13 19:33:14 +0100

    Initial commit

---
+++ ApiDemoExternalModule.php
@@ -0,0 +1,11 @@
+<?php namespace DE\RUB\ApiDemoExternalModule;
+
+use ExternalModules\AbstractExternalModule;
+
+class ApiDemoExternalModule extends AbstractExternalModule {
+
+    function redcap_module_api($action, $payload, $project_id, $user_id, $format, $returnFormat, $csvDelim) {
+        
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# em_api_demo
-An example module to demonstrate module API services.
+# External Module API Demo
+
+An example module to demonstrate module API services through the `redcap_module_api` hook.
